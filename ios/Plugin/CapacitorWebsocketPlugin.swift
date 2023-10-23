75e6e1c0bba8380e95d89008efcfbb0a9363b45d
Zayed CHAREF <zayed.charef@gmail.com>
2023-10-23 12:44:48 +0200

    fix: basic error handling, adding missing removeAllListener method

--- ios/Plugin/CapacitorWebsocketPlugin.swift
+++ ios/Plugin/CapacitorWebsocketPlugin.swift
@@ -1,70 +1,97 @@
-import Foundation
 import Capacitor
+import Foundation
 import Network
 import Starscream
+
 @objc(CapacitorWebsocketPlugin)
 public class CapacitorWebsocketPlugin: CAPPlugin {
-    var connections: [String: SocketConnection] = [:]
+  var connections: [String: SocketConnection] = [:]
 
-    @objc public func build(_ call: CAPPluginCall) {
-        guard let url = call.getString("url") else {
-            call.reject("Must provide a socket URL")
-            return
-        }
-
-        guard let name = call.getString("name") else {
-            call.reject("Must provide a socket name")
-            return
-        }
-
-        guard let socketURL = URL(string: url) else { call.reject("Couldnt make socket: url parse error"); return }
-
-        var req = URLRequest(url: socketURL);
-        
-        if let headers = call.getObject("headers") {
-            for (key, value) in headers {
-                req.addValue(value as! String, forHTTPHeaderField: key)
-            }
-        }
-        
-        connections[name] = SocketConnection.init(name: name, plugin: self, request: req)
-        call.resolve()
+  @objc public func build(_ call: CAPPluginCall) {
+    guard let url = call.getString("url") else {
+      call.reject("Must provide a socket URL")
+      return
     }
 
-    @objc public func applyListeners(_ call: CAPPluginCall) {
-        guard let connection = getSocket(call) else { return }
-        connection.bind()
-        call.resolve()
+    guard let name = call.getString("name") else {
+      call.reject("Must provide a socket name")
+      return
     }
 
-    @objc public func connect(_ call: CAPPluginCall) {
-        guard var connection = getSocket(call) else { return }
-        connection.connect()
-        call.resolve()
+    guard let socketURL = URL(string: url) else {
+      call.reject("Couldnt make socket: url parse error")
+      return
     }
 
-    @objc public func disconnect(_ call: CAPPluginCall) {
-        guard var connection = getSocket(call) else { return }
-        connection.disconnect()
-        call.resolve()
+    var req = URLRequest(url: socketURL)
+
+    if let headers = call.getObject("headers") {
+      for (key, value) in headers {
+        req.addValue(value as! String, forHTTPHeaderField: key)
+      }
     }
 
-    @objc public func send(_ call: CAPPluginCall) {
-        guard let connection = getSocket(call) else { return }
-        guard let message = call.getString("data") else {
-            call.reject("Must provide data to send")
-            return
-        }
-        connection.socket?.write(string: message)
-        call.resolve()
+    connections[name] = SocketConnection.init(name: name, plugin: self, request: req)
+    call.resolve()
+  }
+
+  @objc public func applyListeners(_ call: CAPPluginCall) {
+    guard let connection = getSocket(call) else {
+      call.reject("Connection not found")
+      return
+    }
+    connection.bind()
+    call.resolve()
+  }
+
+  @objc public func connect(_ call: CAPPluginCall) {
+    guard let connection = getSocket(call) else {
+      call.reject("Connection not found")
+      return
+    }
+    connection.connect()
+    call.resolve()
+  }
+
+  @objc public func disconnect(_ call: CAPPluginCall) {
+    guard let connection = getSocket(call) else {
+      call.reject("Connection not found")
+      return
+    }
+    connection.disconnect()
+    call.resolve()
+  }
+
+  @objc public func send(_ call: CAPPluginCall) {
+    guard let connection = getSocket(call) else { return }
+    guard let message = call.getString("data") else {
+      call.reject("Must provide data to send")
+      return
     }
 
-    private func getSocket(_ call: CAPPluginCall) -> SocketConnection? {
-        guard let name = call.getString("name") else {
-            call.reject("Must provide a socket name")
-            return nil
-        }
-
-        return connections[name]
+    do {
+      try connection.socket?.write(string: message)
+    } catch {
+      call.reject("Failed to send message: \(error.localizedDescription)")
+      return
     }
+
+    call.resolve()
+  }
+
+  private func getSocket(_ call: CAPPluginCall) -> SocketConnection? {
+    guard let name = call.getString("name") else {
+      call.reject("Must provide a socket name")
+      return nil
+    }
+
+    return connections[name]
+  }
+
+  @objc public func removeAllListeners(_ call: CAPPluginCall) {
+    guard let connection = getSocket(call) else { return }
+    connection.removeAllListeners()
+    call.resolve()
+  }
+
 }
--- ios/Plugin/WebsocketConnection.swift
+++ ios/Plugin/WebsocketConnection.swift
@@ -5,64 +5,70 @@
 //  Created by Ryan Cockram on 2023-03-16.
 //
 
+import Capacitor
 import Foundation
 import Network
-import Capacitor
 import Starscream
 
 class SocketConnection {
-    private var connected: Bool = false
-    var socket: WebSocket?
-    var pluginContext: CAPPlugin
-    var name: String
-    
-    init(name: String, plugin: CAPPlugin, request: URLRequest) {
-        self.name = name
-        self.pluginContext = plugin
-        self.socket = WebSocket(request: request)
+  private var connected: Bool = false
+  var socket: WebSocket?
+  var pluginContext: CAPPlugin
+  var name: String
+
+  init(name: String, plugin: CAPPlugin, request: URLRequest) {
+    self.name = name
+    self.pluginContext = plugin
+    self.socket = WebSocket(request: request)
+  }
+
+  public func connect() {
+    if !self.connected {
+      self.socket?.connect()
     }
-    
-    public func connect() {
-        if !self.connected {
-            self.socket?.connect()
-        }
+  }
+
+  public func disconnect() {
+    if self.connected {
+      self.socket?.disconnect()
     }
-    
-    public func disconnect() {
-        if self.connected {
-            self.socket?.disconnect()
-        }
+  }
+
+  public func removeAllListeners() {
+    socket?.onEvent = nil
+  }
+
+  public func bind() {
+    socket?.onEvent = { event in
+      switch event {
+      case .connected(_):
+        self.connected = true
+        self.pluginContext.notifyListeners("\(self.name):connected", data: ["iosws": "connected"])
+      case .disconnected(let reason, let code):
+        self.connected = false
+        self.pluginContext.notifyListeners(
+          "\(self.name):disconnected", data: ["code": code, "reason": reason])
+      case .text(let string):
+        self.pluginContext.notifyListeners("\(self.name):message", data: ["data": string])
+      case .binary(let data):
+        self.pluginContext.notifyListeners("\(self.name):message", data: ["data": data])
+      case .ping(_):
+        break
+      case .pong(_):
+        break
+      case .viabilityChanged(_):
+        break
+      case .reconnectSuggested(_):
+        break
+      case .cancelled:
+        self.connected = false
+      case .error(let error):
+        self.connected = false
+        self.pluginContext.notifyListeners(
+          "\(self.name):error", data: ["cause": error ?? "Unknown Error"])
+      case .peerClosed:
+        self.pluginContext.notifyListeners("\(self.name):message", data: ["data": "Peer closed"])
+      }
     }
-    
-    public func bind() {
-        socket?.onEvent = { event in
-            switch event {
-                case .connected(_):
-                    self.connected = true
-                    self.pluginContext.notifyListeners("\(self.name):connected", data: ["iosws": "connected"])
-                case .disconnected(let reason, let code):
-                    self.connected = false
-                    self.pluginContext.notifyListeners("\(self.name):disconnected", data: ["code": code, "reason": reason ])
-                case .text(let string):
-                    self.pluginContext.notifyListeners("\(self.name):message", data: ["data": string])
-                case .binary(let data):
-                    self.pluginContext.notifyListeners("\(self.name):message", data: ["data": data])
-                case .ping(_):
-                    break
-                case .pong(_):
-                    break
-                case .viabilityChanged(_):
-                    break
-                case .reconnectSuggested(_):
-                    break
-                case .cancelled:
-                    self.connected = false
-                case .error(let error):
-                    self.connected = false
-                    self.pluginContext.notifyListeners("\(self.name):error", data: ["cause": error ?? "Unknown Error"])
-                case .peerClosed:
-                    self.pluginContext.notifyListeners("\(self.name):message", data: ["data": "Peer closed"])
-            }
-        }
-    }
+  }
 }
