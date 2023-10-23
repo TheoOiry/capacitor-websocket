45f6e086fa5210b2799a628723f8eec415458ad5
TheoOiry <theo.oiry@yahoo.fr>
2023-10-23 12:10:45 +0200

    fix: android, setup basic error handling

--- android/src/main/java/xyz/miaz/capacitorwebsocket/CapacitorWebsocket.java
+++ android/src/main/java/xyz/miaz/capacitorwebsocket/CapacitorWebsocket.java
@@ -46,7 +46,7 @@ public class CapacitorWebsocket extends Plugin {
             sockets.put(name, new SocketConnection(socket, name));
             call.resolve();
         } catch (Exception e) {
-            call.reject("Couldnt create socket");
+            call.reject("Couldnt create socket, ", e);
         }
     }
 
@@ -58,106 +58,116 @@ public class CapacitorWebsocket extends Plugin {
             return;
         }
 
-        socket.socket.clearListeners();
+        try {
+            socket.socket.clearListeners();
+        } catch (Exception e) {
+            call.reject("Couldnt remove listners: ", e);
+            return;
+        }
 
-        socket.socket.addListener(new WebSocketAdapter() {
-            @Override
-            public void onTextMessage(WebSocket websocket, String message) throws Exception {
-                //super.onTextMessage(websocket, message);
-                JSObject ret = new JSObject();
-                ret.put("data", message);
-                notifyListeners(String.format("%s:message", socket.name), ret);
-            }
+        try {
+            socket.socket.addListener(new WebSocketAdapter() {
+                @Override
+                public void onTextMessage(WebSocket websocket, String message) throws Exception {
+                    //super.onTextMessage(websocket, message);
+                    JSObject ret = new JSObject();
+                    ret.put("data", message);
+                    notifyListeners(String.format("%s:message", socket.name), ret);
+                }
 
-            @Override
-            public void onStateChanged(WebSocket websocket, WebSocketState newState) throws Exception {
-                //super.onStateChanged(websocket, newState);
-                System.out.println(newState);
-                JSObject ret = new JSObject();
-                ret.put("state", newState);
-                notifyListeners(String.format("%s:statechanged", socket.name), ret);
-            }
+                @Override
+                public void onStateChanged(WebSocket websocket, WebSocketState newState) throws Exception {
+                    //super.onStateChanged(websocket, newState);
+                    System.out.println(newState);
+                    JSObject ret = new JSObject();
+                    ret.put("state", newState);
+                    notifyListeners(String.format("%s:statechanged", socket.name), ret);
+                }
 
-            @Override
-            public void onCloseFrame(WebSocket websocket, WebSocketFrame frame) throws Exception {
-                //super.onCloseFrame(websocket, frame);
-                System.out.println("close frame");
-                JSObject ret = new JSObject();
-                ret.put("frame", frame);
-                notifyListeners(String.format("%s:closeframe", socket.name), ret);
-            }
+                @Override
+                public void onCloseFrame(WebSocket websocket, WebSocketFrame frame) throws Exception {
+                    //super.onCloseFrame(websocket, frame);
+                    System.out.println("close frame");
+                    JSObject ret = new JSObject();
+                    ret.put("frame", frame);
+                    notifyListeners(String.format("%s:closeframe", socket.name), ret);
+                }
 
-            @Override
-            public void onConnected(WebSocket websocket, Map<String, List<String>> headers) throws Exception {
-                //super.onConnected(websocket, headers);
-                System.out.println("connected");
-                JSObject ret = new JSObject();
-                ret.put("headers", headers);
-                notifyListeners(String.format("%s:connected", socket.name), ret);
-            }
+                @Override
+                public void onConnected(WebSocket websocket, Map<String, List<String>> headers) throws Exception {
+                    //super.onConnected(websocket, headers);
+                    System.out.println("connected");
+                    JSObject ret = new JSObject();
+                    ret.put("headers", headers);
+                    notifyListeners(String.format("%s:connected", socket.name), ret);
+                }
 
-            @Override
-            public void onDisconnected(WebSocket websocket, WebSocketFrame serverCloseFrame, WebSocketFrame clientCloseFrame, boolean closedByServer) throws Exception {
-                //super.onDisconnected(websocket, serverCloseFrame, clientCloseFrame, closedByServer);
-                System.out.println("disconnected");
-                JSObject ret = new JSObject();
-                ret.put("closedByServer", closedByServer);
-                ret.put("serverCloseFrame", serverCloseFrame);
-                ret.put("clientCloseFrame", clientCloseFrame);
-                notifyListeners(String.format("%s:disconnected", socket.name), ret);
-            }
+                @Override
+                public void onDisconnected(WebSocket websocket, WebSocketFrame serverCloseFrame, WebSocketFrame clientCloseFrame, boolean closedByServer) throws Exception {
+                    //super.onDisconnected(websocket, serverCloseFrame, clientCloseFrame, closedByServer);
+                    System.out.println("disconnected");
+                    JSObject ret = new JSObject();
+                    ret.put("closedByServer", closedByServer);
+                    ret.put("serverCloseFrame", serverCloseFrame);
+                    ret.put("clientCloseFrame", clientCloseFrame);
+                    notifyListeners(String.format("%s:disconnected", socket.name), ret);
+                }
 
-            @Override
-            public void onConnectError(WebSocket websocket, WebSocketException exception) throws Exception {
-                //super.onConnectError(websocket, exception);
-                JSObject ret = new JSObject();
-                ret.put("exception", exception);
-                notifyListeners(String.format("%s:connecterror", socket.name), ret);
-            }
+                @Override
+                public void onConnectError(WebSocket websocket, WebSocketException exception) throws Exception {
+                    //super.onConnectError(websocket, exception);
+                    JSObject ret = new JSObject();
+                    ret.put("exception", exception);
+                    notifyListeners(String.format("%s:connecterror", socket.name), ret);
+                }
 
-            @Override
-            public void onError(WebSocket websocket, WebSocketException cause) throws Exception {
-                //super.onError(websocket, cause);
-                JSObject ret = new JSObject();
-                ret.put("cause", cause);
-                notifyListeners(String.format("%s:error", socket.name), ret);
-            }
+                @Override
+                public void onError(WebSocket websocket, WebSocketException cause) throws Exception {
+                    //super.onError(websocket, cause);
+                    JSObject ret = new JSObject();
+                    ret.put("cause", cause);
+                    notifyListeners(String.format("%s:error", socket.name), ret);
+                }
 
-            @Override
-            public void onMessageError(WebSocket websocket, WebSocketException cause, List<WebSocketFrame> frames) throws Exception {
-                //super.onMessageError(websocket, cause, frames);
-                JSObject ret = new JSObject();
-                ret.put("cause", cause);
-                ret.put("frame", frames);
-                notifyListeners(String.format("%s:messageerror", socket.name), ret);
-            }
+                @Override
+                public void onMessageError(WebSocket websocket, WebSocketException cause, List<WebSocketFrame> frames) throws Exception {
+                    //super.onMessageError(websocket, cause, frames);
+                    JSObject ret = new JSObject();
+                    ret.put("cause", cause);
+                    ret.put("frame", frames);
+                    notifyListeners(String.format("%s:messageerror", socket.name), ret);
+                }
 
-            @Override
-            public void onSendError(WebSocket websocket, WebSocketException cause, WebSocketFrame frame) throws Exception {
-                //super.onSendError(websocket, cause, frame);
-                JSObject ret = new JSObject();
-                ret.put("cause", cause);
-                ret.put("frame", frame);
-                notifyListeners(String.format("%s:senderror", socket.name), ret);
-            }
+                @Override
+                public void onSendError(WebSocket websocket, WebSocketException cause, WebSocketFrame frame) throws Exception {
+                    //super.onSendError(websocket, cause, frame);
+                    JSObject ret = new JSObject();
+                    ret.put("cause", cause);
+                    ret.put("frame", frame);
+                    notifyListeners(String.format("%s:senderror", socket.name), ret);
+                }
 
-            @Override
-            public void onTextMessage(WebSocket websocket, byte[] data) throws Exception {
-                //super.onTextMessage(websocket, data);
-                JSObject ret = new JSObject();
-                ret.put("data", data);
-                notifyListeners(String.format("%s:textmessage", socket.name), ret);
-            }
+                @Override
+                public void onTextMessage(WebSocket websocket, byte[] data) throws Exception {
+                    //super.onTextMessage(websocket, data);
+                    JSObject ret = new JSObject();
+                    ret.put("data", data);
+                    notifyListeners(String.format("%s:textmessage", socket.name), ret);
+                }
 
-            @Override
-            public void onTextMessageError(WebSocket websocket, WebSocketException cause, byte[] data) throws Exception {
-                //super.onTextMessageError(websocket, cause, data);
-                JSObject ret = new JSObject();
-                ret.put("data", data);
-                ret.put("cause", cause);
-                notifyListeners(String.format("%s:textmessageerror", socket.name), ret);
-            }
-        });
+                @Override
+                public void onTextMessageError(WebSocket websocket, WebSocketException cause, byte[] data) throws Exception {
+                    //super.onTextMessageError(websocket, cause, data);
+                    JSObject ret = new JSObject();
+                    ret.put("data", data);
+                    ret.put("cause", cause);
+                    notifyListeners(String.format("%s:textmessageerror", socket.name), ret);
+                }
+            });
+        } catch (Exception e) {
+            call.reject("Couldnt add listners: ", e);
+            return;
+        }
         call.resolve();
     }
 
@@ -170,8 +180,13 @@ public class CapacitorWebsocket extends Plugin {
         }
 
         if (!socket.connected) {
-            socket.socket.connect();
-            socket.connected = true;
+            try {
+                socket.socket.connect();
+                socket.connected = true;
+            } catch (Exception e) {
+                call.reject("Couldnt connect, ", e);
+                return;
+            }
         }
         call.resolve();
     }
@@ -185,8 +200,13 @@ public class CapacitorWebsocket extends Plugin {
         }
 
         if (socket.connected) {
-            socket.socket.disconnect();
-            socket.connected = false;
+            try {
+                socket.socket.disconnect();
+                socket.connected = false;
+            } catch (Exception e) {
+                call.reject("Couldnt disconnect, ", e);
+                return;
+            }
         }
         call.resolve();
     }
@@ -199,7 +219,13 @@ public class CapacitorWebsocket extends Plugin {
             return;
         }
 
-        socket.socket.sendText(call.getString("data"));
+        try {
+            socket.socket.sendText(call.getString("data"));
+        } catch (Exception e) {
+            call.reject("Error sending, ", e);
+            return;
+        }
+
         call.resolve();
     }
 
